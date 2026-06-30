b0e99122643b8008f531ec9c2886a3c4ebf18b88
Varshini janaki k <janunithesh@gmail.com>
2026-06-30 11:46:56 +0530

    notifications.js

--- notification-app-fe/src/api/notifications.js
+++ notification-app-fe/src/api/notifications.js
@@ -1,2 +1,19 @@
-export function fetchNotifications() {
+const BASE_URL =
+  "http://4.224.186.213/evaluation-service/notifications";
+  
+export function fetchNotification({
+    page =1,
+    limit=10,
+    notificationType= "",
+}) {
+    const params = new URLSearchParams({page,limit,});
+    if(notificationType != "All"){
+        params.append("Notification Type:",notificationType);
+    }
+    const response = fetch('${BASE_URL}?${params.toString()}');
+    if(!response.ok){
+        throw new ErrorEvent("Failed occured");
+    }
+    return response.json();
+
 }
